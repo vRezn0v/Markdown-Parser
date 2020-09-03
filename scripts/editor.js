0156c7a9fa563e73b7a6a7ca28c2444d425f1d07
Abhinav <34809845+vRezn0v@users.noreply.github.com>
2020-09-03 10:45:11 +0530

    Save Intercept and Settings Init

--- scripts/editor.js
+++ scripts/editor.js
@@ -50,6 +50,13 @@ window.onload = function () {
     }
     parser();
     mdin.addEventListener('keyup', parser, false);
+    document.addEventListener("keydown", function(e) {
+        if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+          e.preventDefault();
+          saveProgress();
+          alert("saved");
+        }
+    }, false);
 }
 
 window.onbeforeunload = () => {
